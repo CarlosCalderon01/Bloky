2617cff7f10da15c682c2e8dfa4e1cb9da97713b
NewLine2012xD <Carlos.Calderon01@outlook.com>
2024-02-13 08:29:17 -0500

    Se implmenta recursividad

--- Manager/main.py
+++ Manager/main.py
@@ -2,18 +2,17 @@ import os
 
 
 def cambiar_nombres_carpeta(ruta_carpeta):
-    lista_archivos = os.listdir(ruta_carpeta)
+    for ruta_principal, carpetas, archivos in os.walk(ruta_carpeta):
+        for contador, nombre_archivo in enumerate(archivos, start=1):
+            nombre, formato = os.path.splitext(nombre_archivo)
 
-    for contador, nombre_archivo in enumerate(lista_archivos, start=1):
-        nombre, formato = os.path.splitext(nombre_archivo)
+            ruta_antiguo = os.path.join(ruta_principal, nombre_archivo)
+            nombre_nuevo = f"{formato[1:]}_{contador}{formato}"
+            ruta_nuevo = os.path.join(ruta_principal, nombre_nuevo)
 
-        ruta_antiguo = os.path.join(ruta_carpeta, nombre_archivo)
-        nombre_nuevo = f"{formato[1:]}_{contador}{formato}"
-        ruta_nuevo = os.path.join(ruta_carpeta, nombre_nuevo)
+            os.rename(ruta_antiguo, ruta_nuevo)
 
-        os.rename(ruta_antiguo, ruta_nuevo)
-
-        print(f"Renombrado: {nombre_archivo} a {nombre_nuevo}")
+            print(f"Renombrado: {nombre_archivo} a {nombre_nuevo}")
 
 
 ruta_carpeta = "ruta_path"
