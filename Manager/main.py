5af381283bedaa55ae66cb57ae3194e6ae260477
NewLine2012xD <Carlos.Calderon01@outlook.com>
2024-02-13 08:32:50 -0500

    Se agrega un stop y un contador

--- Manager/main.py
+++ Manager/main.py
@@ -1,20 +1,50 @@
 import os
+import time
+
+# Get Acount Number
 
 
-def cambiar_nombres_carpeta(ruta_carpeta):
+def obtener_contador(ruta_contador):
+    try:
+        with open(ruta_contador, 'r') as file:
+            return int(file.read())
+    except FileNotFoundError:
+        return 1
+    except ValueError:
+        return 1
+
+# Save Acount Number
+
+
+def guardar_contador(ruta_contador, contador):
+    with open(ruta_contador, 'w') as file:
+        file.write(str(contador))
+
+# Change Name Files
+
+
+def cambiar_nombres_carpeta(ruta_carpeta, ruta_contador):
+    contador = obtener_contador(ruta_contador)
+
     for ruta_principal, carpetas, archivos in os.walk(ruta_carpeta):
-        for contador, nombre_archivo in enumerate(archivos, start=1):
+        for nombre_archivo in archivos:
             nombre, formato = os.path.splitext(nombre_archivo)
-
             ruta_antiguo = os.path.join(ruta_principal, nombre_archivo)
             nombre_nuevo = f"{formato[1:]}_{contador}{formato}"
             ruta_nuevo = os.path.join(ruta_principal, nombre_nuevo)
 
             os.rename(ruta_antiguo, ruta_nuevo)
-
             print(f"Renombrado: {nombre_archivo} a {nombre_nuevo}")
 
+            contador += 1
+            guardar_contador(ruta_contador, contador)
 
-ruta_carpeta = "ruta_path"
+            time.sleep(0.1)
 
-cambiar_nombres_carpeta(ruta_carpeta)
+
+# Rutas
+ruta_carpeta = "D:\\Variados_IMG"
+ruta_contador = "C:\\Users\\carlo\\Documents\\dev\\Bloky\\Temp\\contador.txt"
+
+# Funcion principal
+cambiar_nombres_carpeta(ruta_carpeta, ruta_contador)
